1cafc1b00ab19da41b117348843367e453b2a46f
Jim Burton <jim618@fastmail.co.uk>
2015-05-12 16:54:44 +0100

    also broadcast unknown tx at start up

--- core/src/main/java/org/bitcoinj/core/Wallet.java
+++ core/src/main/java/org/bitcoinj/core/Wallet.java
@@ -4795,7 +4795,8 @@ public class Wallet extends BaseTaggableObject implements Serializable, BlockCha
         // Don't hold the wallet lock whilst doing this, so if the broadcaster accesses the wallet at some point there
         // is no inversion.
         for (Transaction tx : toBroadcast) {
-            checkState(tx.getConfidence().getConfidenceType() == ConfidenceType.PENDING);
+            checkState(tx.getConfidence().getConfidenceType() == ConfidenceType.PENDING ||
+                    tx.getConfidence().getConfidenceType() == ConfidenceType.UNKNOWN);
             // Re-broadcast even if it's marked as already seen for two reasons
             // 1) Old wallets may have transactions marked as broadcast by 1 peer when in reality the network
             //    never saw it, due to bugs.
